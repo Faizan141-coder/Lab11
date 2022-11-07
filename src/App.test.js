60591381515301474461059cd3149bc6c9a442aa
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2022-11-07 13:19:14 +0500

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId('comp');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId("comp");
+  expect(linkElement).toHaveTextContent("Hello World");
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId("comp");
+  expect(linkElement).toHaveClass("App");
+});
